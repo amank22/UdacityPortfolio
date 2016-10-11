b05070786b8a5bbfdd6d1ad0dfe26d1aa7e54e4d
Aman Kapoor <kapoor.aman22@gmail.com>
2016-10-11 21:28:14 +0530

    Updated hard-coded and generalised in styles

--- app/src/main/java/com/aman/udacityportfolio/MenuActivity.java
+++ app/src/main/java/com/aman/udacityportfolio/MenuActivity.java
@@ -11,6 +11,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MenuActivity extends AppCompatActivity {
 
 
